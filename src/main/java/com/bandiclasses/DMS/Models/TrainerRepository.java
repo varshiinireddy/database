396b3019b1e5a980c5486ff815db1b2a7224a317
Varshini Reddy <137301727+varshiinireddy@users.noreply.github.com>
2024-04-19 17:02:01 -0500

    Add files via upload

---
+++ src/main/java/com/bandiclasses/DMS/Models/TrainerRepository.java
@@ -0,0 +1,9 @@
+package com.bandiclasses.DMS.Models;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bandiclasses.DMS.Models.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
